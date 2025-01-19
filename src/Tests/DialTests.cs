5c899ed2e86d9fd40c798bc385b2cb6425915ed0
blaw <github@brettlawrence.cc>
2025-01-19 16:37:14 +0000

    bunit tests

---
+++ src/Tests/DialTests.cs
@@ -0,0 +1,213 @@
+﻿using BlazorVintageKit;
+using BlazorVintageKit.Components;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class DialTests : BunitTestContext
+    {
+        [TestMethod]
+        public void DialRadiusZero()
+        {
+            var options = new DialOptions()
+            {
+                DialRadius = 0
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void DialRangeZero()
+        {
+            var options = new DialOptions()
+            {
+                DialRange = 0
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void FaceAngleZero()
+        {
+            var options = new DialOptions()
+            {
+                FaceArcAngle = -1
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void IndicatorCountZero()
+        {
+            var options = new DialOptions()
+            {
+                IndicatorCount = 0
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void SubIndicatorCountZero()
+        {
+            var options = new DialOptions()
+            {
+                SubIndicatorCount = 0
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void ValueDivisorZero()
+        {
+            var options = new DialOptions()
+            {
+                ValueDivisor = 0
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+        }
+
+        [TestMethod]
+        public void RiskModeOffRendersNoRiskElements()
+        {
+            var options = new DialOptions()
+            {
+                RiskMode = false
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+
+            //risk mode is not rendered
+            Action act = () => cut.Find("#g_risk");
+            act.Should().Throw<ElementNotFoundException>();
+        }
+
+        [TestMethod]
+        public void InnerCircleModeOnRendersInnerCircleElements()
+        {
+            var options = new DialOptions()
+            {
+                InnerCircleMode = true
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+
+            //inner circle mode is rendered
+            cut.Find("#c_innerCircle");
+        }
+
+        [TestMethod]
+        public void SmithsModeOnRendersSmithsElements()
+        {
+            var options = new DialOptions()
+            {
+                SmithsMode = true
+            };
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+
+            //smith mode is rendered
+            cut.Find("#g_smithsRing");
+        }
+
+
+        [TestMethod]
+        public void DefaultRendersCorrectly()
+        {
+            var options = new DialOptions();
+
+            // Arrange & Act
+            var cut = RenderComponent<Dial>(parameters => parameters
+              .Add(p => p.Options, options)
+            );
+
+            // Assert that the elements are present
+            var outerCircle = cut.Find("#c_outer");
+            outerCircle.MarkupMatches(@$"<circle id=""c_outer"" cx=""50"" cy=""50"" r=""{options.DialRadius}"" fill=""{options.DialColour}"" />");
+
+            //risk mode is default
+            cut.Find("#g_risk");
+
+            //inner circle mode is false by default
+            Action act = () => cut.Find("#c_innerCircle");
+            act.Should().Throw<ElementNotFoundException>();
+
+            //inner ring mode is false by default
+            act = () => cut.Find("#g_innerRing");
+            act.Should().Throw<ElementNotFoundException>();
+
+            //smiths ring mode is false by default
+            act = () => cut.Find("#g_smithsRing");
+            act.Should().Throw<ElementNotFoundException>();
+
+            //indicators are default
+            cut.Find("#g_indicators");
+
+            //text indicators are default
+            cut.Find("#g_textIndicators");
+
+            //text labels are default
+            cut.Find("#t_makers");
+            cut.Find("#t_key");
+
+            //rest mode is false by default
+            act = () => cut.Find("#g_resetButton");
+            act.Should().Throw<ElementNotFoundException>();
+
+            act = () => cut.Find("#p_reset");
+            act.Should().Throw<ElementNotFoundException>();
+
+            act = () => cut.Find("#g_reset");
+            act.Should().Throw<ElementNotFoundException>();
+
+            //pointer is default
+            cut.Find("#g_pointer");
+            cut.Find("#p_pointer");
+
+            //boss is default
+            cut.Find("#c_boss");
+            cut.Find("#c_bossCenter");
+
+            //rim is default
+            cut.Find("#c_rim");
+        }
+    }
+}
---
+++ src/Tests/SeventeenSegmentDisplayTests.cs
@@ -0,0 +1,57 @@
+﻿using BlazorVintageKit.Components;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class SeventeenSegmentDisplayTests : BunitTestContext
+    {
+        [TestMethod]
+        public void ValidateTestSanity()
+        {
+            // Arrange
+            var cut = RenderComponent<SeventeenSegmentDisplay>(parameters => parameters
+              .Add(p => p.Character, 'A')
+            );
+                        
+            Action act = () => cut.Find("#p1.on");
+
+            //Act & Assert that a missing element throws the expected exception
+            act.Should().Throw<Bunit.ElementNotFoundException>();
+        }
+
+        [TestMethod]
+        public void ARendersCorrectly()
+        {
+            // Arrange & Act
+            var cut = RenderComponent<SeventeenSegmentDisplay>(parameters => parameters
+              .Add(p => p.Character, 'A')
+            );
+
+            // Assert that the correct segments are lit
+            cut.Find("#p1.off");
+            cut.Find("#p2.off");
+            cut.Find("#p4.off");
+            cut.Find("#p3.off");
+            cut.Find("#p5.on");
+            cut.Find("#p6.on");
+            cut.Find("#p7.off");
+            cut.Find("#p8.off");
+            cut.Find("#p9.on");
+            cut.Find("#p10.on");
+            cut.Find("#p11.on");
+            cut.Find("#p12.on");
+            cut.Find("#p13.on");
+            cut.Find("#p14.on");
+            cut.Find("#p15.off");
+            cut.Find("#p16.off");
+            cut.Find("#p17.off");
+
+        }
+    }
+}
